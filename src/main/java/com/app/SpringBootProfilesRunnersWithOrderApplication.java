3961fc47b1e5080b274dbe42ae1791e71c872f2d
valekar <valekar@sagar>
2019-06-07 15:32:11 +0530

    All file Added

---
+++ src/main/java/com/app/SpringBootProfilesRunnersWithOrderApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProfilesRunnersWithOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProfilesRunnersWithOrderApplication.class, args);
+	}
+
+}
